df959e5823f8a31731838729bcdaa21a1fbc77f2
CarlDom92438-32 <108300658+CarlDom92438-32@users.noreply.github.com>
2022-08-24 12:31:37 -0500

    Update registerVote.php

--- registerVote.php
+++ registerVote.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/CarlDom92438-32/AppCasillasKotlin/dbConnection.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/AppCasillasKotlin/dbConnection.php';
 
 $cve = $_GET["CVE"];
 
